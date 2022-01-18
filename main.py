a9fd42e2c54575919a33162e7cc93b088825c6f2
Savanna <97067396+Savanna15@users.noreply.github.com>
2022-01-18 23:49:17 +0000

    testing

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ def generate_marvel(user_input):
   ]
   return random.choice(marvel)
 
+  #team iron man
+  #team cap
+
 def generate_kpop(user_input):
   kpop = [
     "Ult boy group?",
@@ -23,12 +26,19 @@ def generate_kpop(user_input):
     "That's so cool!",
     "SM, YG, or JYPE",
     "SOTY?",
-    "Best generation of kpop?",
+    "Gen 2, 3, or 4?",
     "Favorite solo artisti?",
     "No way, same!",
     "100% agree with you!",
   ]
   return random.choice(kpop)
+
+  #gen 2
+  #gen 3
+  #gen 4
+  #SM
+  #JYPE
+  #YG
 
 def init_chat():
   quit_character = 'q'
@@ -39,10 +49,14 @@ def init_chat():
     while user_input != quit_character:
       #Ask the user for more input, then use that in your response
       user_input = input(generate_marvel(user_input) + "\n")
+      #if user inputs iron man start using list of questions relating to team iron man
+      #if cap list realting to team cap
   elif(user_input == "Kpop"):
     while user_input != quit_character:
       #Ask the user for more input, then use that in your response
       user_input = input(generate_kpop(user_input) + "\n")
+      #if gen ask from gen's list
+      #if company ask from company's list
 
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,7 @@
+##import unitteset
+##import main
+
+##class TestInitChat(unitteset.TestCase):
+  
+  ##def test_init_chat(self):
+    ##self.assertEquals()
